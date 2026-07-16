777c91777c8e510c2ce47b137050e9005730abc4
Ankit Gupta <app045774@gmail.com>
2026-07-16 12:53:04 +0530

    ANk1252

--- hidroDss/src/components/body/body.jsx
+++ hidroDss/src/components/body/body.jsx
@@ -3,10 +3,18 @@ import "./body.css";
 
 function Body() {
     return (
+<<<<<<< HEAD
         <div className="body-container">
             <section className="bg-white border border-slate-200 shadow-sm rounded-xl py-6 px-6 w-full h-full"><TimeSeries />
             </section>
 
+=======
+        <div className="flex">
+            <section className="bg-white border border-slate-200 shadow-sm rounded-xl py-12 px-6 max-w-5xl mx-auto my-8"><TimeSeries /></section>
+             
+            
+            
+>>>>>>> 58701db (ANk1252)
 
             {/* <section><Map/></section>
                     <section></section>
