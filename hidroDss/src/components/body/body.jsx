35f4fa6c587fe86775f3985e615c8f49048a9a91
Ankit Gupta <app045774@gmail.com>
2026-07-17 11:20:52 +0530

    Ank1120

--- hidroDss/src/components/body/body.jsx
+++ hidroDss/src/components/body/body.jsx
@@ -1,40 +1,23 @@
 import TimeSeries from "../charts/TimeSeries";
-import Map from "../Map/map";
 import "./body.css";
-import Datasetsummary from "../DatasetSummary/datasetsummary";
-import Chrips from "../chirps/Chrips"
-import Rainfall from "../RainfallStatistics/Rainfall"
-import RainfallAnomaly from "../RainfallAnomaly/RainfallAnomaly"
+
 function Body() {
     return (
-
         <div className="flex">
-           
-           <section><Chrips/> </section>
-            <section className="bg-white border border-slate-200 shadow-sm rounded-xl p-2 w-l lg:w-2/3 h-[500px]" >
-                <Map />
-
-            </section>
-            <section className="bg-white border border-slate-200 shadow-sm rounded-xl py-6 px-6 w-full h-full">
-                <TimeSeries />
-            </section>
-            <section>
-                <Datasetsummary />
-            </section>
-            <section><Rainfall/></section>
-            <section><RainfallAnomaly/></section>
+            <section className="bg-white border border-slate-200 shadow-sm rounded-xl py-12 px-6 max-w-5xl mx-auto my-8"><TimeSeries /></section>
 
 
 
 
-
-
-
-
-
-
-
+            {/* <section><Map/></section>
+                    <section></section>
+                    <section></section>
+                    <section></section>
+                    <section></section>
+                    <section></section>
+                    <section></section>
+                    <section></section> */}
         </div>
-    );
+    )
 }
 export default Body;
